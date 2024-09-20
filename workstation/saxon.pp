4ea1402aa37665b37e1aecde28ac155006f3ebae
Adam Retter <adam.retter@googlemail.com>
2024-09-20 10:58:14 +0100

    TEMP

--- workstation/saxon.pp
+++ workstation/saxon.pp
@@ -13,7 +13,7 @@ exec { 'download-saxon-zip':
   command => "/usr/bin/curl -L https://github.com/Saxonica/Saxon-HE/releases/download/SaxonHE${saxon_download_version}/SaxonHE${saxon_download_version}J.zip -o /tmp/SaxonHE${saxon_download_version}J.zip",
   creates => "/opt/saxon-${saxon_jar_version}/saxon-he-${saxon_jar_version}.jar",
   require => [
-    Package['curl'],
+    # Package['curl'],
     File["/opt/saxon-${saxon_jar_version}"]
   ],
 }
@@ -22,7 +22,7 @@ exec { 'install-saxon':
   command => "/usr/bin/unzip /tmp/SaxonHE${saxon_download_version}J.zip -d /opt/saxon-${saxon_jar_version}",
   creates => "/opt/saxon-${saxon_jar_version}/saxon-he-${saxon_jar_version}.jar",
   require => [
-    Package['zip'],
+    # Package['zip'],
     Exec['download-saxon-zip']
   ],
 }
