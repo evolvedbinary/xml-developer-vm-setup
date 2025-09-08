cityEHR/cityehr-workshop-vm-setup

Refactor to use a module for creating desktop shortcuts

workstation/x-chrome.pp
@@ -18,38 +18,18 @@ package { 'google-chrome-stable':
   ],
 }
 
-file { 'google-chrome-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/google-chrome.desktop",
-  source  => '/usr/share/applications/google-chrome.desktop',
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Chrome':
+  shortcut_source => '/usr/share/applications/google-chrome.desktop',
+  user            => $custom_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 138,
+  },
+  require         => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
-    Package['google-chrome-stable'],
-  ],
-}
-
-exec { 'gvfs-trust-google-chrome-desktop-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/google-chrome.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/google-chrome.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['google-chrome-desktop-shortcut'],
-}
-
-ini_setting { 'google-chrome-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'google-chrome.desktop',
-  setting => 'pos',
-  value   => '@Point(139 138)',
-  require => [
     File['desktop-items-0'],
-    File['google-chrome-desktop-shortcut'],
+    Package['google-chrome-stable'],
   ],
 }

workstation/x-desktop-shortcuts.pp
@@ -46,51 +46,22 @@ exec { 'download-eb-favicon-logo':
   ],
 }
 
-$eb_desktop_shortcut =  @("EB_SHORTCUT_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Name=Evolved Binary
-  Exec=/usr/bin/google-chrome-stable https://www.evolvedbinary.com
-  StartupNotify=true
-  Terminal=false
-  Icon=/home/${custom_user}/.local/share/icons/eb-favicon-logo.svg
-  Type=Application
-  | EB_SHORTCUT_EOF
-
-file { 'eb-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/evolved-binary.desktop",
-  content => $eb_desktop_shortcut,
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Evolved Binary':
+  application_path => '/usr/bin/google-chrome-stable https://www.evolvedbinary.com',
+  application_icon => "/home/${custom_user}/.local/share/icons/eb-favicon-logo.svg",
+  startup_notify   => true,
+  user             => $custom_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 393,
+    y        => 264,
+  },
+  require          => [
     Package['desktop'],
-    File['custom_user_desktop_folder'],
     Package['google-chrome-stable'],
-    Exec['download-eb-favicon-logo'],
-  ],
-}
-
-exec { 'gvfs-trust-eb-desktop-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/evolved-binary.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/evolved-binary.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['eb-desktop-shortcut'],
-}
-
-ini_setting { 'eb-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'evolved-binary.desktop',
-  setting => 'pos',
-  value   => '@Point(393 264)',
-  require => [
+    File['custom_user_desktop_folder'],
     File['desktop-items-0'],
-    File['eb-desktop-shortcut'],
+    Exec['download-eb-favicon-logo'],
   ],
 }
 
@@ -105,50 +76,21 @@ exec { 'download-ohi-logo':
   ],
 }
 
-$ohi_desktop_shortcut =  @("OHI_SHORTCUT_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Name=Open Health Informatics
-  Exec=/usr/bin/google-chrome-stable https://openhealthinformatics.com
-  StartupNotify=true
-  Terminal=false
-  Icon=/home/${custom_user}/.local/share/icons/ohi-logo.png
-  Type=Application
-  | OHI_SHORTCUT_EOF
-
-file { 'ohi-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/open-health-informatics.desktop",
-  content => $ohi_desktop_shortcut,
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Open Health Informatics':
+  application_path => '/usr/bin/google-chrome-stable https://openhealthinformatics.com',
+  application_icon => "/home/${custom_user}/.local/share/icons/ohi-logo.png",
+  startup_notify   => true,
+  user             => $custom_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 393,
+    y        => 138,
+  },
+  require          => [
     Package['desktop'],
-    File['custom_user_desktop_folder'],
     Package['google-chrome-stable'],
-    Exec['download-ohi-logo'],
-  ],
-}
-
-exec { 'gvfs-trust-ohi-desktop-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/open-health-informatics.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/open-health-informatics.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['ohi-desktop-shortcut'],
-}
-
-ini_setting { 'ohi-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'open-health-informatics.desktop',
-  setting => 'pos',
-  value   => '@Point(393 138)',
-  require => [
+    File['custom_user_desktop_folder'],
     File['desktop-items-0'],
-    File['ohi-desktop-shortcut'],
+    Exec['download-ohi-logo'],
   ],
 }

workstation/x-firefox.pp
@@ -7,49 +7,20 @@ package { 'firefox':
   require => Package['desktop'],
 }
 
-$firefox_desktop_shortcut =  @("FIREFOX_SHORTCUT_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Name=Firefox
-  Exec=/usr/bin/firefox
-  StartupNotify=true
-  Terminal=false
-  Icon=/usr/share/icons/hicolor/128x128/apps/firefox.png
-  Type=Application
-  | FIREFOX_SHORTCUT_EOF
-
-file { 'firefox-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/firefox.desktop",
-  content => $firefox_desktop_shortcut,
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Firefox':
+  application_path => '/usr/bin/firefox',
+  application_icon => '/usr/share/icons/hicolor/128x128/apps/firefox.png',
+  startup_notify   => true,
+  user             => $custom_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 12,
+  },
+  require          => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
-    Package['firefox'],
-  ],
-}
-
-exec { 'gvfs-trust-firefox-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/firefox.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/firefox.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['firefox-desktop-shortcut'],
-}
-
-ini_setting { 'firefox-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'firefox.desktop',
-  setting => 'pos',
-  value   => '@Point(139 12)',
-  require => [
     File['desktop-items-0'],
-    File['firefox-desktop-shortcut'],
+    Package['firefox'],
   ],
 }

workstation/x-intellij.pp
@@ -31,50 +31,19 @@ file { '/opt/idea-IC':
   require => File["/opt/idea-IC-${intellij_idea_version}"],
 }
 
-$intellij_desktop_shortcut = @("INTELLIJ_DESKTOP_ENTRY_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Type=Application
-  Name=IntelliJ IDEA CE
-  Exec=/opt/idea-IC/bin/idea.sh
-  Icon=/opt/idea-IC/bin/idea.svg
-  Terminal=false
-  StartupNotify=false
-  GenericName=IntelliJ IDEA CE
-  | INTELLIJ_DESKTOP_ENTRY_EOF
-
-file { 'intellij-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/intellij.desktop",
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  content => $intellij_desktop_shortcut,
-  require => [
+xdesktop::shortcut { 'IntelliJ IDEA CE':
+  application_path => '/opt/idea-IC/bin/idea.sh',
+  application_icon => '/opt/idea-IC/bin/idea.svg',
+  user             => $custom_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 266,
+    y        => 390,
+  },
+  require          => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
-    File['/opt/idea-IC'],
-  ],
-}
-
-exec { 'gvfs-trust-intellij-desktop-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/intellij.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/intellij.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['intellij-desktop-shortcut'],
-}
-
-ini_setting { 'intellij-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'intellij.desktop',
-  setting => 'pos',
-  value   => '@Point(266 390)',
-  require => [
     File['desktop-items-0'],
-    File['intellij-desktop-shortcut'],
+    Package['firefox'],
   ],
 }

workstation/x-oxygenxml.pp
@@ -63,51 +63,20 @@ file { '/opt/oxygen':
   require => File["/opt/oxygen-${oxygen_version}"],
 }
 
-$oxygen_desktop_shortcut = @("OXYGEN_DESKTOP_ENTRY_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Type=Application
-  Name=Oxygen XML Editor
-  Exec=/opt/oxygen/oxygen.sh
-  Icon=/opt/oxygen/Oxygen128.png
-  Terminal=false
-  StartupNotify=false
-  GenericName=Oxygen XML Editor
-  | OXYGEN_DESKTOP_ENTRY_EOF
-
-file { 'oxygen-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/oxygen.desktop",
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  content => $oxygen_desktop_shortcut,
-  require => [
+xdesktop::shortcut { 'Oxygen XML Editor':
+  application_path => '/opt/oxygen/oxygen.sh',
+  application_icon => '/opt/oxygen/Oxygen128.png',
+  user             => $custom_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 642,
+  },
+  require          => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
-    File['/opt/oxygen']
-  ],
-}
-
-exec { 'gvfs-trust-oxygen-desktop-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/oxygen.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/oxygen.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['oxygen-desktop-shortcut'],
-}
-
-ini_setting { 'oxygen-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'oxygen.desktop',
-  setting => 'pos',
-  value   => '@Point(139 642)',
-  require => [
     File['desktop-items-0'],
-    File['oxygen-desktop-shortcut'],
+    File['/opt/oxygen'],
   ],
 }
 

workstation/x-vscode.pp
@@ -25,38 +25,18 @@ file_line { 'vscode-no-open-folder':
   require => Package['code'],
 }
 
-file { 'vscode-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${custom_user}/Desktop/code.desktop",
-  source  => '/usr/share/applications/code.desktop',
-  owner   => $custom_user,
-  group   => $custom_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Code':
+  shortcut_source => '/usr/share/applications/code.desktop',
+  user            => $custom_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 768,
+  },
+  require         => [
     Package['desktop'],
     File['custom_user_desktop_folder'],
-    File_line['vscode-no-open-folder'],
-  ],
-}
-
-exec { 'gvfs-trust-vscode-desktop-shortcut':
-  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/code.desktop metadata::trusted true",
-  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/code.desktop | /usr/bin/grep trusted",
-  user        => $custom_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['vscode-desktop-shortcut'],
-}
-
-ini_setting { 'vscode-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'code.desktop',
-  setting => 'pos',
-  value   => '@Point(139 768)',
-  require => [
     File['desktop-items-0'],
-    File['vscode-desktop-shortcut'],
+    File_line['vscode-no-open-folder'],
   ],
 }

evolvedbinary/xml-developer-vm-setup

workstation/x-chrome.pp
@@ -18,38 +18,18 @@ package { 'google-chrome-stable':
   ],
 }
 
-file { 'google-chrome-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/google-chrome.desktop",
-  source  => '/usr/share/applications/google-chrome.desktop',
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Chrome':
+  shortcut_source => '/usr/share/applications/google-chrome.desktop',
+  user            => $default_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 113,
+    y        => 12,
+  },
+  require         => [
     Package['desktop'],
     File['default_user_desktop_folder'],
-    Package['google-chrome-stable'],
-  ],
-}
-
-exec { 'gvfs-trust-google-chrome-desktop-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/google-chrome.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/google-chrome.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['google-chrome-desktop-shortcut'],
-}
-
-ini_setting { 'google-chrome-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'google-chrome.desktop',
-  setting => 'pos',
-  value   => '@Point(113 12)',
-  require => [
     File['desktop-items-0'],
-    File['google-chrome-desktop-shortcut'],
+    Package['google-chrome-stable'],
   ],
 }

workstation/x-desktop-shortcuts.pp
@@ -46,52 +46,23 @@ exec { 'download-existdb-x-logo':
   ],
 }
 
-$existdb_desktop_shortcut =  @("EXISTDB_SHORTCUT_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Name=eXist-db Dashboard
-  Exec=/usr/bin/google-chrome-stable http://localhost:8080
-  StartupNotify=true
-  Terminal=false
-  Icon=/home/${default_user}/.local/share/icons/existdb-x.png
-  Type=Application
-  | EXISTDB_SHORTCUT_EOF
-
-file { 'existdb-dashboard-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/existdb-dashboard.desktop",
-  content => $existdb_desktop_shortcut,
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'eXist-db Dashboard':
+  application_path => '/usr/bin/google-chrome-stable http://localhost:8080',
+  application_icon => "/home/${default_user}/.local/share/icons/existdb-x.png",
+  startup_notify   => true,
+  user             => $default_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 214,
+    y        => 115,
+  },
+  require          => [
     Package['desktop'],
-    File['default_user_desktop_folder'],
-    Service['existdb'],
     Package['google-chrome-stable'],
-    Exec['download-existdb-x-logo'],
-  ],
-}
-
-exec { 'gvfs-trust-existdb-dashboard-desktop-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/existdb-dashboard.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/existdb-dashboard.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['existdb-dashboard-desktop-shortcut'],
-}
-
-ini_setting { 'existdb-dashboard-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'existdb-dashboard.desktop',
-  setting => 'pos',
-  value   => '@Point(214 115)',
-  require => [
+    File['default_user_desktop_folder'],
     File['desktop-items-0'],
-    File['existdb-dashboard-desktop-shortcut'],
+    Exec['download-existdb-x-logo'],
+    Service['existdb'],
   ],
 }
 
@@ -106,50 +77,21 @@ exec { 'download-eb-favicon-logo':
   ],
 }
 
-$training_course_desktop_shortcut =  @("TRAINING_COURSE_SHORTCUT_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Name=The Complete XML Developer - Slides
-  Exec=/usr/bin/google-chrome-stable https://static.evolvedbinary.com/cxd/
-  StartupNotify=true
-  Terminal=false
-  Icon=/home/${default_user}/.local/share/icons/eb-favicon-logo.svg
-  Type=Application
-  | TRAINING_COURSE_SHORTCUT_EOF
-
-file { 'training-course-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/training-course.desktop",
-  content => $training_course_desktop_shortcut,
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'The Complete XML Developer - Slides':
+  application_path => '/usr/bin/google-chrome-stable https://static.evolvedbinary.com/cxd/',
+  application_icon => "/home/${default_user}/.local/share/icons/eb-favicon-logo.svg",
+  startup_notify   => true,
+  user             => $default_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 214,
+    y        => 12,
+  },
+  require          => [
     Package['desktop'],
-    File['default_user_desktop_folder'],
     Package['google-chrome-stable'],
-    Exec['download-eb-favicon-logo'],
-  ],
-}
-
-exec { 'gvfs-trust-training-course-desktop-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/training-course.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/training-course.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['training-course-desktop-shortcut'],
-}
-
-ini_setting { 'training-course-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'training-course.desktop',
-  setting => 'pos',
-  value   => '@Point(214 12)',
-  require => [
+    File['default_user_desktop_folder'],
     File['desktop-items-0'],
-    File['training-course-desktop-shortcut'],
+    Exec['download-eb-favicon-logo'],
   ],
 }

workstation/x-firefox.pp
@@ -7,38 +7,20 @@ package { 'firefox':
   require => Package['desktop'],
 }
 
-file { 'firefox-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/firefox.desktop",
-  source  => '/usr/share/applications/firefox.desktop',
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Firefox':
+  application_path => '/usr/bin/firefox',
+  application_icon => '/usr/share/icons/hicolor/128x128/apps/firefox.png',
+  startup_notify   => true,
+  user             => $default_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 113,
+    y        => 115,
+  },
+  require          => [
     Package['desktop'],
     File['default_user_desktop_folder'],
-    Package['firefox'],
-  ],
-}
-
-exec { 'gvfs-trust-firefox-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/firefox.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/firefox.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['firefox-desktop-shortcut'],
-}
-
-ini_setting { 'firefox-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'firefox.desktop',
-  setting => 'pos',
-  value   => '@Point(113 115)',
-  require => [
     File['desktop-items-0'],
-    File['firefox-desktop-shortcut'],
+    Package['firefox'],
   ],
 }

workstation/x-intellij.pp
@@ -31,47 +31,19 @@ file { '/opt/idea-IC':
   require => File["/opt/idea-IC-${intellij_idea_version}"],
 }
 
-file { 'intellij-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/intellij.desktop",
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  content => "[Desktop Entry]
-Version=1.0
-Type=Application
-Name=IntelliJ IDEA CE
-Exec=/opt/idea-IC/bin/idea.sh
-Icon=/opt/idea-IC/bin/idea.svg
-Terminal=false
-StartupNotify=false
-GenericName=IntelliJ IDEA CE
-",
-  require => [
+xdesktop::shortcut { 'IntelliJ IDEA CE':
+  application_path => '/opt/idea-IC/bin/idea.sh',
+  application_icon => '/opt/idea-IC/bin/idea.svg',
+  user             => $default_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 113,
+    y        => 218,
+  },
+  require          => [
     Package['desktop'],
     File['default_user_desktop_folder'],
-    File['/opt/idea-IC'],
-  ],
-}
-
-exec { 'gvfs-trust-intellij-desktop-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/intellij.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/intellij.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['intellij-desktop-shortcut'],
-}
-
-ini_setting { 'intellij-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'intellij.desktop',
-  setting => 'pos',
-  value   => '@Point(113 218)',
-  require => [
     File['desktop-items-0'],
-    File['intellij-desktop-shortcut'],
+    File['/opt/idea-IC'],
   ],
 }

workstation/x-oxygenxml.pp
@@ -64,51 +64,20 @@ file { '/opt/oxygen':
   require => File["/opt/oxygen-${oxygen_version}"],
 }
 
-$oxygen_desktop_shortcut = @("OXYGEN_DESKTOP_ENTRY_EOF"/L)
-  [Desktop Entry]
-  Version=1.0
-  Type=Application
-  Name=Oxygen XML Editor
-  Exec=/opt/oxygen/oxygen.sh
-  Icon=/opt/oxygen/Oxygen128.png
-  Terminal=false
-  StartupNotify=false
-  GenericName=Oxygen XML Editor
-  | OXYGEN_DESKTOP_ENTRY_EOF
-
-file { 'oxygen-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/oxygen.desktop",
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  content => $oxygen_desktop_shortcut,
-  require => [
+xdesktop::shortcut { 'Oxygen XML Editor':
+  application_path => '/opt/oxygen/oxygen.sh',
+  application_icon => '/opt/oxygen/Oxygen128.png',
+  user             => $default_user,
+  position         => {
+    provider => 'lxqt',
+    x        => 139,
+    y        => 218,
+  },
+  require          => [
     Package['desktop'],
     File['default_user_desktop_folder'],
-    File['/opt/oxygen']
-  ],
-}
-
-exec { 'gvfs-trust-oxygen-desktop-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/oxygen.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/oxygen.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['oxygen-desktop-shortcut'],
-}
-
-ini_setting { 'oxygen-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'oxygen.desktop',
-  setting => 'pos',
-  value   => '@Point(139 218)',
-  require => [
     File['desktop-items-0'],
-    File['oxygen-desktop-shortcut'],
+    File['/opt/oxygen'],
   ],
 }
 

workstation/x-vscode.pp
@@ -25,38 +25,18 @@ file_line { 'vscode-no-open-folder':
   require => Package['code'],
 }
 
-file { 'vscode-desktop-shortcut':
-  ensure  => file,
-  path    => "/home/${default_user}/Desktop/code.desktop",
-  source  => '/usr/share/applications/code.desktop',
-  owner   => $default_user,
-  group   => $default_user,
-  mode    => '0644',
-  require => [
+xdesktop::shortcut { 'Code':
+  shortcut_source => '/usr/share/applications/code.desktop',
+  user            => $default_user,
+  position        => {
+    provider => 'lxqt',
+    x        => 113,
+    y        => 527,
+  },
+  require         => [
     Package['desktop'],
     File['default_user_desktop_folder'],
-    File_line['vscode-no-open-folder'],
-  ],
-}
-
-exec { 'gvfs-trust-vscode-desktop-shortcut':
-  command     => "/usr/bin/gio set /home/${default_user}/Desktop/code.desktop metadata::trusted true",
-  unless      => "/usr/bin/gio info --attributes=metadata::trusted /home/${default_user}/Desktop/code.desktop | /usr/bin/grep trusted",
-  user        => $default_user,
-  environment => [
-    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
-  ],
-  require     => File['vscode-desktop-shortcut'],
-}
-
-ini_setting { 'vscode-desktop-shortcut-position':
-  ensure  => present,
-  path    => "/home/${default_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
-  section => 'code.desktop',
-  setting => 'pos',
-  value   => '@Point(113 527)',
-  require => [
     File['desktop-items-0'],
-    File['vscode-desktop-shortcut'],
+    File_line['vscode-no-open-folder'],
   ],
 }